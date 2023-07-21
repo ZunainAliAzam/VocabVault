3a0bf1518e0a33a08da995d31c4388138f8ddde2
ZunainAliAzam <zunainazam456@gmail.com>
2023-07-22 01:09:48 +0500

    updated: app.js + index.html

--- app.js
+++ app.js
@@ -1,28 +1,42 @@
 function Search(entry) {
-    const url= 'https://twinword-word-graph-dictionary.p.rapidapi.com/definition/?entry=' + entry
+    const urlDefinition =
+        "https://twinword-word-graph-dictionary.p.rapidapi.com/definition/?entry=" +
+        entry;
+    const urlAssociatedWords =
+        "https://twinword-word-graph-dictionary.p.rapidapi.com/association/?entry=" +
+        entry;
     const options = {
-        method: 'GET',
+        method: "GET",
         headers: {
-            'X-RapidAPI-Key': '57f76065fbmsh38574de6c6fda3dp128d13jsn36f6adf3c736',
-            'X-RapidAPI-Host': 'twinword-word-graph-dictionary.p.rapidapi.com'
-        }
+            "X-RapidAPI-Key": "57f76065fbmsh38574de6c6fda3dp128d13jsn36f6adf3c736",
+            "X-RapidAPI-Host": "twinword-word-graph-dictionary.p.rapidapi.com",
+        },
     };
-    
-    fetch(url, options)
-        .then(response => response.json())
-        .then((response,assoc_word) => {
+
+    fetch(urlDefinition, options)
+        .then((response) => response.json())
+        .then((response) => {
             console.log(response);
-            console.log(assoc_word)
             entryheading.innerHTML = response.entry;
             noun.innerHTML = response.meaning.noun;
             verb.innerHTML = response.meaning.verb;
         })
-        .catch( (error) => {
-        console.error(error);
+        .catch((error) => {
+            console.error(error);
+        });
+    fetch(urlAssociatedWords, options)
+        .then((response) => response.json())
+        .then((response) => {
+            // console.log(response);
+            console.log(response);
+            assoc_word.innerHTML = response.assoc_word;
         })
+        .catch((error) => {
+            console.error(error);
+        });
 }
 
-search.addEventListener('click', (e) => {
+search.addEventListener("click", (e) => {
     e.preventDefault();
-    Search(searchinput.value)
+    Search(searchinput.value);
 });
